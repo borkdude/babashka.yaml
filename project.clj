b72d1368c1ee7b4274a4d3a9ab9dda373b924c50
Michiel Borkent <michielborkent@gmail.com>
2020-03-30 22:07:05 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-commons/clj-yaml "0.7.0"
+(defproject borkdude/clj-yaml "0.8.0-SNAPSHOT"
   :description "YAML encoding and decoding for Clojure using SnakeYAML"
   :url "https://github.com/clj-commons/clj-yaml"
   :license {:name "Eclipse Public License - v 1.0"
@@ -13,4 +13,8 @@
   :dependencies
   [[org.yaml/snakeyaml "1.25"]
    [frankiesardo/linked "1.3.0"]]
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]]}}
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
